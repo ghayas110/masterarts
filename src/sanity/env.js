a11dac3af4ebec1ba85c8070b2d6c8eae5bfba19
Ghayas Ali <55618250+ghayas110@users.noreply.github.com>
2025-03-29 03:54:28 +0500

    ss

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,5 +1,5 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-28'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = "production";
+export const projectId = "yj93u1bs";
